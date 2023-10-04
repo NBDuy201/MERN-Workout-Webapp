2e9872af9d1281f746d3e57b0899ade4981d3c49
NBDuy <nbduy2001@gmail.com>
2023-10-04 09:36:55 +0700

    update api config FE

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,5 @@
-export const API_URL =
-  import.meta.env.VITE_APP_PRODUCTION_DOMAIN ??
-  `http://localhost:${import.meta.env.VITE_APP_BACKEND_PORT}`;
+const isDev = import.meta.env.DEV;
+
+export const API_URL = !isDev
+  ? import.meta.env.VITE_APP_PRODUCTION_DOMAIN
+  : `http://localhost:${import.meta.env.VITE_APP_BACKEND_PORT}`;
